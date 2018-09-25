11b15e9d252b2fe36eba3d577158435651ed06fa
蓝宝丹 <744492528@qq.com>
2018-09-25 17:59:45 +0800

    feat:图片压缩

--- config/webpack.base.conf.js
+++ config/webpack.base.conf.js
@@ -33,12 +33,19 @@ module.exports = {
           {
             loader: 'url-loader', //limit 图片大小的衡量，进行base64处理
             options: {
-              limit: 1 * 1024,
-              outputPath: "images"
+              name: '[name].[ext]',
+              limit: 10*1024,
+              outputPath: 'images'
             },
           },
+          { // 压缩图片
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            }
+          }
         ],
-      },
+      }
     ],
   },
   plugins: [
