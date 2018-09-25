cd1aba896f923d986fe373cb6def19ffdd80dc42
蓝宝丹 <744492528@qq.com>
2018-09-25 15:26:53 +0800

    feat: add cash

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 // import React from 'react';
 import { render } from 'react-dom';
 import Loadable from 'react-loadable';
+import registerServiceWorker from './registerServiceWorker';
 import { createStoresFromState } from './store/utils';
 import createApp from './createApp';
 
@@ -13,5 +14,6 @@ Loadable.preloadReady().then(() => {
   const application = createApp({ stores });
   if (typeof document !== 'undefined') {
     render(application, ROOT);
+    registerServiceWorker();
   }
 });
--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,22 @@ import Loadable from 'react-loadable';
 import LoadingComponent from './components/LoadingComponent';
 import App from './containers/App';
 
-import AsyncTodos from './containers/Todos';
-import AsyncOther from './containers/Other';
+// import AsyncTodos from './containers/Todos';
+// import AsyncOther from './containers/Other';
 
 const COMMIT_DELAY = 200;
 
-// const AsyncTodos = Loadable({
-//   loader: () => import('./containers/Todos'),
-//   loading: LoadingComponent,
-//   delay: COMMIT_DELAY,
-// });
+const AsyncTodos = Loadable({
+  loader: () => import('./containers/Todos'),
+  loading: LoadingComponent,
+  delay: COMMIT_DELAY,
+});
 
-// const AsyncOther = Loadable({
-//   loader: () => import('./containers/Other'),
-//   loading: LoadingComponent,
-//   delay: COMMIT_DELAY,
-// });
+const AsyncOther = Loadable({
+  loader: () => import('./containers/Other'),
+  loading: LoadingComponent,
+  delay: COMMIT_DELAY,
+});
 
 export default [
   {
